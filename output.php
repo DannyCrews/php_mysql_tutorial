4aef7c52ec30ac06616768560b941777063bd659
Dan Crews <dancrews@me.com>
2014-03-04 20:20:45 -0500

    Basic php file

---
+++ output.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Output</title>
+    </head>
+    <body>
+        <?php
+        echo "This is echoed out.";
+        echo ("<br/>This is also echoed out.");
+        print("<br/>Look ma! I didn't use echo, I used print!");
+        print "<br/>Print also works without the aid of ().";
+        printf("<br/>Mark is %d years old.", 37);
+        printf("<br/>The interest rate is %0.2f", 8.993124645);
+        ?>
+    </body
+</html>
